6cfaf14ce7af8588ef0af871a62da426ec2b3dfd
jpniewski <jpniewski@debian>
2025-03-07 05:05:47 +0100

    final version

--- scrap.py
+++ scrap.py
@@ -7,6 +7,7 @@ from transformers import pipeline
 from tqdm import tqdm
 from datetime import datetime
 from duckduckgo_search import DDGS
+from urllib.parse import urlparse
 
 date = datetime.now().strftime('%Y-%m-%d')
 
@@ -19,7 +20,9 @@ desc_class = 'model-card-content'
 
 gpu_av = torch.cuda.is_available()
 print("Cuda ", 'un' if not gpu_av else '', 'available', sep='')
-device = -int(gpu_av)
+device = 'cuda' if gpu_av else 'cpu'
+
+print("Loading summarizer...")
 
 summarizer = pipeline(
     "summarization", 
@@ -27,14 +30,20 @@ summarizer = pipeline(
     torch_dtype=torch.float16,
     device=device
 )
+print("Summarizer loaded")
 
-subpath = '_posts/' 
-if os.path.exists(subpath):
-    shutil.rmtree(subpath)
+def clear_make_path(pth):
+    if os.path.exists(pth):
+        shutil.rmtree(pth)
+    os.makedirs(pth)
 
-os.makedirs(subpath)
+post_pth = '_posts/' 
+images_pth = 'assets/images'
 
-subpath += date + '-'
+clear_make_path(post_pth)
+clear_make_path(images_pth)
+
+post_pth += date + '-'
 post_pref = '/models/' 
 
 def make_md(title, layout='page', background=""):
@@ -69,18 +78,28 @@ def get_info(model):
     tags = page_soup.find_all('div', class_='tag tag-white')
 
     desc = page_soup.find_all('div', class_='model-card-content')
-    desc = ' '.join([i.text for i in desc])[:3500]
+    desc = ' '.join([i.text for i in desc])
+    desc = desc[:2400] if len(desc) > 2400 else desc
 
     license = page_soup.find('span', string='License:')
-    license = license.next_sibling.next_sibling.text.strip()
+    if license is not None:
+        license = license.next_sibling.next_sibling.text.strip()
+    else:
+        license = "ND"
 
-    s_summary = summarizer(desc, max_length=100, min_length=50, do_sample=False)
-    summary = summarizer(desc, max_length=500, min_length=200, do_sample=False)
+    s_summary = summarizer(desc, max_length=50, min_length=20, do_sample=False)
+    summary = summarizer(desc, max_length=400, min_length=100, do_sample=False)
 
     search_phraze = model.find('h4').get_text(strip=True).split('/')[1]
-    img = DDGS().images(search_phraze, max_results=1, safesearch='on')[0]['image']
-    ddgs_links = DDGS().text(search_phraze, max_results=5, safesearch='on')
+    ddgs_links = DDGS().text(search_phraze, max_results=5, safesearch='on', region='pl-pl')
     ddgs_links = [i['href'] for i in ddgs_links]
+
+    img_url = DDGS().images(search_phraze, max_results=1, safesearch='on', region='pl-pl')[0]['image']
+    img_res = requests.get(img_url)
+    img_pth = images_pth + '/' + str(search_phraze).replace('.', '-').replace('/', '-').lower() + '.jpg'
+
+    with open(img_pth, 'wb') as f:
+        f.write(img_res.content)    
 
     data = {}
 
@@ -92,7 +111,7 @@ def get_info(model):
     data['likes'] = page_soup.find('button', title='See users who liked this repository').text.strip()    
     data['summary'] = summary[0]['summary_text']
     data['short-summary'] = s_summary[0]['summary_text']
-    data['img'] = img
+    data['img'] = '/' + img_pth
     data['links'] = filter(lambda h: 'huggingface.co' not in h, ddgs_links)
 
     return data
@@ -105,12 +124,14 @@ list_md = make_md('Lista modeli z 🤗')
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-models_classes = soup.find_all('article', class_=model_class)
+limit = 25
+models = soup.find_all('article', class_=model_class)
+models_classes = models[:limit]
 
-i = 0
+print("Generating list and posts...")
 for model in tqdm(models_classes):
-    i += 1
-    if i == 3:
+    limit -= 1
+    if limit == 0:
         break
 
     data = get_info(model)
@@ -122,11 +143,13 @@ for model in tqdm(models_classes):
     sub_md += md_text('**Transformers**: ' + ('✅' if data['transformers'] else '❌'))
     sub_md += md_link('🤗 link', data['url'])
     sub_md += md_text('**Summary**:\n' + data['summary'])
-    sub_md += md_text('**Useful links**:')
+    sub_md += md_text('**Useful links**:\n')
+
     for link in data['links']:
-        sub_md += md_link(link, link)
+        domain = urlparse(link).netloc
+        sub_md += f'\n - [{domain}/...]({link})'
     
-    sub_name = subpath + data['name'].replace('/', '_') + '.md'
+    sub_name = post_pth + data['name'].replace('/', '_') + '.md'
     post_url = post_pref + data['name'].replace('/', '_')
 
     with open(sub_name, 'w') as f:
@@ -143,3 +166,5 @@ for model in tqdm(models_classes):
 
 with open('posts.md', 'w') as f:
     print(list_md, file=f)
+
+print("Generation completed!")
